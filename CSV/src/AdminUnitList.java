72793d07a81e71df43e957a2950b4d64d62984ad
Bartek <bartekko@student.agh.edu.pl>
2017-12-14 12:14:26 +0100

    Lab9 - some new methods

--- CSV/src/AdminUnitList.java
+++ CSV/src/AdminUnitList.java
@@ -6,9 +6,12 @@ import java.util.*;
 public class AdminUnitList {
     List<AdminUnit> units = new ArrayList<>();
 
-    AdminUnitList(List<AdminUnit> units){this.units = units;}
+    AdminUnitList(List<AdminUnit> units) {
+        this.units = units;
+    }
 
-    public AdminUnitList() { }
+    public AdminUnitList() {
+    }
 
     public void read(String filename) throws IOException {
         CSVReader reader = new CSVReader(filename, ",", true);
@@ -24,7 +27,7 @@ public class AdminUnitList {
             double x4 = reader.getDouble("x4");
             double y4 = reader.getDouble("y4");
             double[] points = new double[4];
-            points = FindXY(x1,y1,x2,y2,x3,y3,x4,y4);
+            points = FindXY(x1, y1, x2, y2, x3, y3, x4, y4);
             long id = reader.getLong("id");
             String name = reader.get("name");
             int admin_level = reader.getInt("admin_level");
@@ -42,17 +45,17 @@ public class AdminUnitList {
     }
 
 
-    double[] FindXY(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4){ //Find xmin, ymin, xmax, ymax
+    double[] FindXY(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) { //Find xmin, ymin, xmax, ymax
         double[] points = new double[4];
-        points[0] = Math.min(Math.min(x1,x2), Math.min(x3,x4));
-        points[1] = Math.min(Math.min(y1,y2), Math.min(y3,y4));
-        points[2] = Math.max(Math.max(x1,x2), Math.max(x3,x4));
-        points[3] = Math.max(Math.max(y1,y2), Math.max(y3,y4));
+        points[0] = Math.min(Math.min(x1, x2), Math.min(x3, x4));
+        points[1] = Math.min(Math.min(y1, y2), Math.min(y3, y4));
+        points[2] = Math.max(Math.max(x1, x2), Math.max(x3, x4));
+        points[3] = Math.max(Math.max(y1, y2), Math.max(y3, y4));
 
         return points;
     }
 
-    void setParents(Map<AdminUnit, Long> admin2parentId, Map<Long, AdminUnit> id2admin){
+    void setParents(Map<AdminUnit, Long> admin2parentId, Map<Long, AdminUnit> id2admin) {
         for (AdminUnit au : units) {
             long id_parent = admin2parentId.get(au);
             if (id_parent != 0) // if parent ID == 0 it means the AdminUnit has no parent
@@ -61,28 +64,28 @@ public class AdminUnitList {
         }
     }
 
-    void setChildren(){
-        for(AdminUnit au : units) {
-            if(au.parent != null)
+    void setChildren() {
+        for (AdminUnit au : units) {
+            if (au.parent != null)
                 au.parent.children.add(au);
         }
     }
 
-    void list(PrintStream out){
-        for(AdminUnit au : units) out.printf(au.toString());
+    void list(PrintStream out) {
+        for (AdminUnit au : units) out.printf(au.toString());
     }
 
-     void getWKTofUnit(String unit_name){
-        for(AdminUnit au : units)
-            if(Objects.equals(au.name, unit_name)){
-                System.out.printf(Locale.US,"LINESTRING(%f %f, %f %f, %f %f, %f %f, %f %f)",
+    void getWKTofUnit(String unit_name) {
+        for (AdminUnit au : units)
+            if (Objects.equals(au.name, unit_name)) {
+                System.out.printf(Locale.US, "LINESTRING(%f %f, %f %f, %f %f, %f %f, %f %f)",
                         au.bbox.xmin, au.bbox.ymin, au.bbox.xmax, au.bbox.ymin, au.bbox.xmax, au.bbox.ymax, au.bbox.xmin, au.bbox.ymax, au.bbox.xmin, au.bbox.ymin);
                 break;
             }
     }
 
-    void list(PrintStream out,int offset, int limit ){
-        for(int i=0; i<limit; i++)
+    void list(PrintStream out, int offset, int limit) {
+        for (int i = 0; i < limit; i++)
             out.print(this.units.get(offset + i - 1).toString());
     }
 
@@ -96,49 +99,78 @@ public class AdminUnitList {
         return ret;
     }
 
-    AdminUnit selectByName(String pattern){
-        for(AdminUnit au : this.units) {
+    AdminUnit selectByName(String pattern) {
+        for (AdminUnit au : this.units) {
             if (au.name.equals(pattern))
                 return au;
         }
         return null;
     }
 
-    private void fixMissingValues(AdminUnit au){
-        if(au.parent == null) {
+    private void fixMissingValues(AdminUnit au) {
+        if (au.parent == null) {
             au.density = 0;
             au.population = 10;
-        }else if(au.parent.density == 0 || Double.isNaN(au.parent.density))
+        } else if (au.parent.density == 0 || Double.isNaN(au.parent.density))
             fixMissingValues(au.parent);
         au.density = au.parent.density;
         au.population = au.area * au.density;
 
     }
-    public void fixAllMissingValues(){
-        for(AdminUnit au : units){
-            if(au.density == 0 || Double.isNaN(au.density))
+
+    public void fixAllMissingValues() {
+        for (AdminUnit au : units) {
+            if (au.density == 0 || Double.isNaN(au.density))
                 fixMissingValues(au);
         }
     }
 
     AdminUnitList getNeighbors(AdminUnit unit, double maxdistance) throws EmptyBoundingBox {
         List<AdminUnit> neighbors = new ArrayList<>();
-        if(unit.parent == null || unit.parent.parent == null){ // Województa, powiaty i jednostki dla których parent == null albo parent.parent == null
-            for(AdminUnit au : this.units)
-                if(au.adminLevel == unit.adminLevel && !au.name.equals(unit.name) && unit.bbox.intersects(au.bbox)) neighbors.add(au);
-        }
-        else if(unit.adminLevel == 7){ //Gminy
-            for(AdminUnit au : unit.parent.parent.children)
-                for(AdminUnit au1 : au.children)
-                    if(au1.adminLevel == unit.adminLevel && !au1.name.equals(unit.name) && unit.bbox.intersects(au1.bbox)) neighbors.add(au1);
-        }
-        else{ //Miejscowości
+        if (unit.parent == null || unit.parent.parent == null) { // Województa, powiaty i jednostki dla których parent == null albo parent.parent == null
+            for (AdminUnit au : this.units)
+                if (au.adminLevel == unit.adminLevel && !au.name.equals(unit.name) && unit.bbox.intersects(au.bbox))
+                    neighbors.add(au);
+        } else if (unit.adminLevel == 7) { //Gminy
+            for (AdminUnit au : unit.parent.parent.children)
+                for (AdminUnit au1 : au.children)
+                    if (au1.adminLevel == unit.adminLevel && !au1.name.equals(unit.name) && unit.bbox.intersects(au1.bbox))
+                        neighbors.add(au1);
+        } else { //Miejscowości
             for (AdminUnit au : unit.parent.parent.children)
                 for (AdminUnit au1 : au.children)
                     if (au1.adminLevel == 8 && !au1.name.equals(unit.name) && (unit.bbox.intersects(au1.bbox) || unit.bbox.distanceTo(au1.bbox) < maxdistance))
                         neighbors.add(au1);
         }
         return new AdminUnitList(neighbors);
+    }
+
+
+    public class SortByName implements Comparator<AdminUnit> {
+        public int compare(AdminUnit o1, AdminUnit o2) {
+            return o1.name.compareTo(o2.name);
+        }
+    }
+
+
+    AdminUnitList sortInplaceByName() {
+        units.sort(new SortByName());
+        return this;
+    }
+
+    AdminUnitList sortInplaceByArea(){
+        units.sort(new Comparator<AdminUnit>() {
+            @Override
+            public int compare(AdminUnit o1, AdminUnit o2) {
+                return Double.compare(o1.area, o2.area);
+            }
+        });
+    return this;
+    }
+
+    AdminUnitList sortInplaceByPopulation(){
+        units.sort((o1, o2) -> Double.compare(o1.population, o2.population));
+    return this;
     }
 }
 
--- CSV/src/UnitsBuilder.java
+++ CSV/src/UnitsBuilder.java
@@ -4,6 +4,13 @@ import java.util.Locale;
 
 public class UnitsBuilder {
     public static void main(String[] args) throws IOException, EmptyBoundingBox {
+        AdminUnitList unitlist = new AdminUnitList();
+        unitlist.read("admin-units.csv");
+        unitlist.fixAllMissingValues();
+        AdminUnitList sorted = unitlist.sortInplaceByPopulation();
+        sorted.list(System.out, 1, 100);
+
+
     }
 
 }
